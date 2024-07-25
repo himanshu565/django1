5139e8a027869090c5376b51127e2d023453364b
himanshu565 <101812555+himanshu565@users.noreply.github.com>
2024-07-26 02:31:20 +0530

    gotem

--- dev1/dev1/settings.py
+++ dev1/dev1/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'dev1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / "templates",],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,4 +121,9 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
+#added manually 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    
+]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- dev1/home/views.py
+++ dev1/home/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render,HttpResponse 
+from datetime import datetime
+from home.models import Contact
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello this is home page")
+    context = {
+        "variable":"yoo its working"
+    }
+    return render(request,"index.html",context)
+    # return HttpResponse("hello this is home page")
 
 def about(request):
-    return HttpResponse("hello this is about page")
+    return render(request,"about.html")
+    # return HttpResponse("hello this is about page")
 
 def service(request):
-    return HttpResponse("hello this is service page")
+    return render(request,"services.html")
+    # return HttpResponse("hello this is service page")
 
 def contact(request):
-    return HttpResponse(" this is contact page")
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name,email=email,desc=desc)
+        contact.save()
+    return render(request,"contact.html")
+    # return HttpResponse(" this is contact page")
